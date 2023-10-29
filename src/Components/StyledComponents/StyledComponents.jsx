248064d2a607b65837eb1e0c789830247cd97668
RafatRU42 <rmrafat420@gmail.com>
2023-10-29 12:02:48 +0600

    background completed

---
+++ src/Components/StyledComponents/StyledComponents.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+export const GradientBackground = styled.div`
+  background: linear-gradient(to right, #000046, #1cb5e0);
+   background-size: 400% 400%;
+  animation: gradient 6s ease infinite;
+  height: 100vh;
+  width: 100vw;
+  margin-top:-16px;
+
+`;
